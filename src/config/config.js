35dae9c88ef962327006df17c015127e34eb4977
ulisseslimaa <ulisses.lima741@gmail.com>
2020-01-23 13:58:57 -0300

    alteracao deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-dynamic-require */
-const Config = require(`./env/${(process.env.NODE_ENV || 'production')}.js`);
+const Config = require(`./env/${(process.env.NODE_ENV || 'development')}.js`);
 
 
 module.exports = Config;
--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,7 +1,7 @@
 class Config {
   constructor() {
     this.env = 'production';
-    this.PORT = 3000;
+    this.PORT = process.env.PORT || 3000;
     this.API_BASE = '/api';
     // eslint-disable-next-line max-len
     this.MONGODB_URL = 'mongodb://ufvbhhoydxoqosl5ln2a:6o7NGlDDdqLTOF458Ifj@b6gfpjix1jnhshe-mongodb.services.clever-cloud.com:27017/b6gfpjix1jnhshe';
